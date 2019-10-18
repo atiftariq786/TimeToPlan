3c5bc727d29eac35820ad0d2c935ed1bde6739a3
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-18 16:45:49 -0700

    Component:Carousal slide 1 setting

---
+++ time-to-plan/src/component/Carousel/Carousel.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import Carousel from "react-bootstrap/Carousel"
+import Styles from "./Carousel.module.css";
+
+const ControlledCarousel = () => {
+    const [index, setIndex] = useState(0);
+    const [direction, setDirection] = useState(null);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+      setDirection(e.direction);
+    };
+    //render(<ControlledCarousel />);
+    return (
+      <Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
+      <Carousel.Item>
+      <img 
+        className="d-block w-100 "
+        style={{height:"300px"}}
+        src="https://webstockreview.net/images/planner-clipart-urban-planner-15.png" 
+        alt="First slide"
+      />
+      <Carousel.Caption className={Styles.Craouseltext}>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+
+      </Carousel>
+    );
+  }
+ 
+  
+
+  export default ControlledCarousel;
