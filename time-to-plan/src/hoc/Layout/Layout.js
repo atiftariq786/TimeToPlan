7d70517581fff311f1a85daffee4cb7cf54e7050
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-18 12:55:48 -0700

    hoc: Layout code chunk added

---
+++ time-to-plan/src/hoc/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import Styles from "./Layout.module.css";
+
+class Layout extends Component {
+
+    render(){
+
+        return (
+            <div>
+            Hello World
+            </div>
+        )
+    }
+}
+
+export default Layout;
