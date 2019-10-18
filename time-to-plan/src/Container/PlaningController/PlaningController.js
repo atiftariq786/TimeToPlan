d905b0178948b99c6cade559392b42f97aeb113d
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-18 16:58:29 -0700

    Component:Media code added

--- time-to-plan/src/Container/PlaningController/PlaningController.js
+++ time-to-plan/src/Container/PlaningController/PlaningController.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Carousel from "../../component/Carousel/Carousel";
+import Media from "../../component/Media/MediaStory/MediaStory";
 import Styles from "./PlaningController.module.css";
 
 class PaningController extends Component {
@@ -17,7 +18,7 @@ class PaningController extends Component {
                     <Col sm="12"><div><Carousel/></div></Col>
                 </Row>
                 <Row>
-                    <Col sm="6"><div className={Styles.Temp}>Media Story</div></Col>
+                    <Col sm="6"><div className={Styles.Temp}><Media /></div></Col>
                     <Col sm="6"> <div className={Styles.Temp}>Sketch of list</div></Col>
                 </Row>
                 </Container>
---
+++ time-to-plan/src/component/Media/MediaStory/MediaStory.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const media = (props) => {
+
+    return (
+        
+        <div>
+        <img
+        style={{width: "450px", height:"300px"}} 
+        src="https://media.coschedule.com/uploads/header-layers.gif"></img>
+        </div>
+        
+       
+        
+    );
+};
+export default media;
