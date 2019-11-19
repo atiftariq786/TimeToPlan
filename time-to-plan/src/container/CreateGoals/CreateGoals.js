1c611cdb58058e3817077ce9130bfdb7125eab0f
Qandeel Sajid <atiftariq786@hotmail.com>
2019-11-18 21:05:15 -0800

    Container: CreateGoals component added and code chunk for goals

---
+++ time-to-plan/src/container/CreateGoals/CreateGoals.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {TextArea, Input} from "../../component/Form/Form";
+import Styles from "./CreateGoals.module.css";
+
+
+class CreateGoals extends Component {
+
+    render(){
+        return(
+          
+        )
+    }
+}
+export default CreateGoals;
