36e62b5cd4b5358bac2fd0bd29e636d3af5804cb
Qandeel Sajid <atiftariq786@hotmail.com>
2020-01-22 22:22:34 -0800

    utils heroku url added

--- time-to-plan/src/utils/API.js
+++ time-to-plan/src/utils/API.js
@@ -3,13 +3,13 @@ import axios from "axios";
 export default {
 
     savedStory : function(data){
-        return axios.post("https://git.heroku.com/safe-anchorage-29156.git/api/create-story", data);
+        return axios.post("https://safe-anchorage-29156.herokuapp.com/api/create-story", data);
     },
     getStories : function(){
-        return axios.get("https://git.heroku.com/safe-anchorage-29156.git/api/stories");
+        return axios.get(" https://safe-anchorage-29156.herokuapp.com/api/stories");
     },
     deleteStory : function(id){
-        return axios.delete("https://git.heroku.com/safe-anchorage-29156.git/api/story/" + id);
+        return axios.delete("https://safe-anchorage-29156.herokuapp.com/api/story/" + id);
     },
     updateStory : function(id,data){
         return axios.put("http://localhost:3001/api/story/" + id,data);
