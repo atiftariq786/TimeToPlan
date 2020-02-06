0cee8557fbf657aed8cbfccc614c64a95dee6cdc
Qandeel Sajid <atiftariq786@hotmail.com>
2020-02-05 21:42:53 -0800

    Utils api update

--- time-to-plan/src/utils/API.js
+++ time-to-plan/src/utils/API.js
@@ -5,40 +5,40 @@ axios.defaults.withCredentials = true;
 export default {
 
     savedStory : function(data){
-        return axios.post("//https://safe-anchorage-29156.herokuapp.com/api/create-story", data);
+        return axios.post("https://safe-anchorage-29156.herokuapp.com/api/create-story", data);
     },
     getStories : function(){
-        return axios.get("//https://safe-anchorage-29156.herokuapp.com/api/stories");
+        return axios.get("https://safe-anchorage-29156.herokuapp.com/api/stories");
     },
     deleteStory : function(id){
-        return axios.delete("//https://safe-anchorage-29156.herokuapp.com/api/story/" + id);
+        return axios.delete("https://safe-anchorage-29156.herokuapp.com/api/story/" + id);
     },
     updateStory : function(id,data){
-        return axios.put("//https://safe-anchorage-29156.herokuapp.com/api/story/" + id,data);
+        return axios.put("https://safe-anchorage-29156.herokuapp.com/api/story/" + id,data);
     },
 
     savedGoal : function(data){
         return axios.post("//https://safe-anchorage-29156.herokuapp.com/api/create-goal", data);
     },
     getGoals : function(){
-        return axios.get("//https://safe-anchorage-29156.herokuapp.com/api/goals");
+        return axios.get("https://safe-anchorage-29156.herokuapp.com/api/goals");
     },
     deleteGoal : function(id){
-        return axios.delete("//https://safe-anchorage-29156.herokuapp.com/api/goals/" + id);
+        return axios.delete("https://safe-anchorage-29156.herokuapp.com/api/goals/" + id);
     },
     updateGoal : function(id,data){
-        return axios.put("//https://safe-anchorage-29156.herokuapp.com/api/goals/" + id,data);
+        return axios.put("https://safe-anchorage-29156.herokuapp.com/api/goals/" + id,data);
     },
     
     savedUserSignUp : function(data){
         console.log({data})
-        return axios.post("//https://safe-anchorage-29156.herokuapp.com/auth/register/", data);
+        return axios.post("https://safe-anchorage-29156.herokuapp.com/auth/register/", data);
     },
     savedUserLogin : function(data){
-        return axios.post("//https://safe-anchorage-29156.herokuapp.com/auth/login/", data);
+        return axios.post("https://safe-anchorage-29156.herokuapp.com/auth/login/", data);
     },
     getLogout : function(){
-        return axios.get("//https://safe-anchorage-29156.herokuapp.com/auth/logout/");
+        return axios.get("https://safe-anchorage-29156.herokuapp.com/auth/logout/");
     },
     
 }
